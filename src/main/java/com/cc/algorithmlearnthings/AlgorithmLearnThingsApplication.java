a57752fceb2c644b137a774cf28635cd9d6f7180
chencheng <787915859@qq.com>
2019-03-28 21:53:37 +0800

    Initial commit

---
+++ src/main/java/com/cc/algorithmlearnthings/AlgorithmLearnThingsApplication.java
@@ -0,0 +1,13 @@
+package com.cc.algorithmlearnthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmLearnThingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmLearnThingsApplication.class, args);
+    }
+
+}
