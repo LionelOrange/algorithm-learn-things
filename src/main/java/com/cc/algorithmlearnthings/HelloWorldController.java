3cc890e0ca60f0fa5117a59bbb3ef1ffb9ed3847
chencheng <787915859@qq.com>
2019-03-31 17:29:15 +0800

    插入排序

---
+++ src/main/java/com/cc/algorithmlearnthings/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.cc.algorithmlearnthings;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chencheng
+ * @date 2019/3/31
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "hello";
+    }
+}
